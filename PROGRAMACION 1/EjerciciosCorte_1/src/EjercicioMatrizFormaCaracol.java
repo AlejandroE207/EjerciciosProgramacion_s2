51d9b429248d682b295623c28bf2c77951af9f96
Alejo207 <alejandroriveros2007@hotmail.com>
2022-09-25 16:28:51 -0500

    ejercicio de llenado de matriz en forma de caracol

---
+++ PROGRAMACION 1/EjerciciosCorte_1/src/EjercicioMatrizFormaCaracol.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class EjercicioMatrizFormaCaracol {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("La matriz sera de NxN (cuadrada) Digite el tamaño: ");
+        int n;
+        n = sc.nextInt();
+        int a=0, b=n-1, valor=0;
+        int[][] matriz = new int [n][n];
+       
+        for(int j=0;j<matriz.length;j++){
+            for(int i=a; i<=b;i++){
+                matriz[a][i] = valor++;
+            }
+            for(int i=a+1; i<=b;i++){
+                matriz[i][b] = valor++;
+            }
+            for(int i=b-1; i>=a;i--){
+                matriz[b][i] = valor++;
+            }
+            for(int i=b-1; i>=a+1;i--){
+                matriz[i][a] = valor++;
+            }
+            a++;b--;
+        }
+        
+        for(int i = 0; i<matriz.length;i++){
+            for(int j=0; j<matriz[i].length;j++){
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println("");
+        }
+       
+    }
+    
+}
