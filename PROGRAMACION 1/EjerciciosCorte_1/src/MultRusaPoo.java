cc8f34901619b15094d71e0aa56e590c169b27ef
Alejandro Espinosa <alejandroriveros2007@hotmail.com>
2022-11-02 17:34:19 -0500

    hh

---
+++ PROGRAMACION 1/EjerciciosCorte_1/src/MultRusaPoo.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class MultRusaPoo {
+    int n;
+    int m;
+    
+    public MultRusaPoo(int n,int m){
+        this.n=n;
+        this.m=m;
+    }
+    
+    public int multiplicacion(){
+        int sum=0;
+        do{
+            if(n%2==1){
+                sum+=m;
+            }
+            n=n/2;
+            m=m*2;
+        }
+        while(n>=1);
+        return sum;
+    }
+    
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int n,m;
+        System.out.print("Digite el valor del multiplicando: ");
+        n=sc.nextInt();
+        System.out.print("Digite el valor del multiplicador: ");
+        m=sc.nextInt();
+        
+        MultRusaPoo mult = new MultRusaPoo(n,m);
+        
+        System.out.println("El resultado de la operacion "+n+" x "+m+" es: "+mult.multiplicacion());
+    }
+}
+
---
+++ PROGRAMACION 1/EjerciciosCorte_1/src/VectorNumPrim.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class VectorNumPrim {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int num = 100;
+        int prim=0;
+        System.out.print("Digite el tamaño del vector: ");
+        n=sc.nextInt();
+        int vector [] = new int [n];
+        
+        for(int j=2;j<100;j++){
+            for(int i=2;i<j;i++){
+                if(j%i ==0){
+                    
+                }
+            }
+            System.out.println(prim);
+        }
+        
+    }
+}
