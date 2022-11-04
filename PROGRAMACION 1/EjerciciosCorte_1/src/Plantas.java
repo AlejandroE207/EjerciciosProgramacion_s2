e69fcf3e26d4adfe1cb689222d59da4e227ec531
Alejandro Espinosa <alejandroriveros2007@hotmail.com>
2022-11-04 08:43:56 -0500

    Ajustes ejercicio plantas

--- PROGRAMACION 1/EjerciciosCorte_1/src/Plantas.java
+++ PROGRAMACION 1/EjerciciosCorte_1/src/Plantas.java
@@ -1,6 +1,7 @@
 public class Plantas { 
     String nombre;
     float tamaño,crecimiento;
+    int dias;
 
     public Plantas(String nombre, float tamaño, float crecimiento) {
         this.nombre = nombre;
@@ -11,43 +12,35 @@ public class Plantas {
       int dias=1;
       float tamaFinP1=p1.tamaño,tamaFinP2=p2.tamaño,tamaFinP3=p3.tamaño;
       boolean tbP1=true, tbP2=true, tbP3=true;
-
       
-        while ((tamaFinP1<=30) || (tamaFinP2<=30) || ( tamaFinP3<=30)){
+        while ((tamaFinP1<=30) && (tamaFinP2<=30) && ( tamaFinP3<=30)){
             dias ++;
             tamaFinP1=(tamaFinP1*p1.crecimiento)+tamaFinP1;
+        }
+        dias=1;
+        while(tamaFinP2!=30){
+            dias++;
             tamaFinP2=(tamaFinP2*p2.crecimiento)+tamaFinP2;
+            p2.dias=dias;
+        }
+        dias=1;
+        while(tamaFinP3!=30){
+            dias++;
             tamaFinP3=p3.crecimiento+tamaFinP3;
-            if (tamaFinP1==30){
-                System.out.println("La planta " +p1.nombre+" ha lledado primero a los 30 cm");
-                System.out.println("En "+dias+ "Dias" );
-                break;
-            }
-            if (tamaFinP2==30){
-                
-                System.out.println("La planta " +p2.nombre+" ha lledado primero a los 30 cm");
-                System.out.println("En "+dias+ "Dias" );
-                break;
-               
-                
-            }
-            if (tamaFinP3==30){
-                
-                System.out.println("La planta " +p3.nombre+" ha lledado primero a los 30 cm");
-                System.out.println("En "+dias+  "  Dias" );
-                break;
-                
-                
-            }
-        } 
-
+            p3.dias=dias;
+        }
+     }
+     
+    static void GetMenor(Plantas p1,Plantas p2,Plantas p3){
+        int primero,segundo,tercero;
     }
+    
     public static void main(String[] args) {
 
       Plantas p1= new Plantas("Clementina",(float) 10, (float) 0.01); 
       Plantas p2= new Plantas("Crisis", (float)20, (float) 0.005); 
       Plantas p3= new Plantas("Lucho", (float)5, (float) 0.5); 
       Crecimiento(p1,p2,p3);
-
+      
     }
 }
